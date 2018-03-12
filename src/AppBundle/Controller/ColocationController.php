03fcb987467cf6189398ad2c51e0ee7a9f9cf30f
PierreBizos <pierre.bizos@etu.univ-nantes.fr>
2018-03-12 19:17:03 +0100

    "probleme de route"

---
+++ src/AppBundle/Controller/ColocationController.php
@@ -0,0 +1,43 @@
+<?php
+	
+	namespace AppBundle\Controller;
+
+	use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+	use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+	use Symfony\Component\HttpFoundation\Request;
+	use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+	use AppBundle\Form\ColocatairesType;
+	use AppBundle\Form\ColocationsType;
+	use AppBundle\Form\ActiviteType;
+	use AppBundle\Entity\Colocataires;
+	use AppBundle\Entity\Colocations;
+	use AppBundle\Entity\Activite;
+
+	
+	
+	
+	/**
+	* @Route("/{_locale}/Colocation")
+	*/
+	class ColocationController extends Controller
+	{
+		/**
+		* @Route("/Coloc", name="Ajoutcoloc")
+		* @return \Symfony\Component\HttpFoundaztion\Response
+		* @throws \LogicException
+		*/
+		public function colocAjout(Request $request){
+			// if(!isset($_POST['coloc'])){
+				$Coloc=new Colocations();
+				$form = $this->createForm(ColocationsType::class,$Coloc,[ 'action'=>$this->generateUrl('Coloc'),]);
+				$form->handleRequest($request);
+				if(!$form->isSubmitted() || !$form->isValid()){
+					return $this->render('colocation/add.html.twig',['coloc_form'=>$form->createView(),]);
+				}
+			// }
+			
+				
+			
+		}
+	}
+	
